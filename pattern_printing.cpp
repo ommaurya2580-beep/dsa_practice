1eed24603cc8254d7e0d394e8c293668e246541e
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-25 21:27:28 +0530

    pattern printing

---
+++ pattern_printing.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int i,j,num;
+    cout<<"enter length of pattern";
+    cin>>num;
+    for(i=0;i<=num;i++)
+    {
+        for(j=0;j<=num;j++)
+        {
+            cout<<"*";
+
+        }
+        cout<<" \n";
+    }
+return 0;
+}
