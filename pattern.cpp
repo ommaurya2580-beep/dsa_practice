ac28ba6b0b91abaf2098dfa138a1903d4a306926
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-25 17:20:07 +0530

    pattern

---
+++ pattern.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main (){
+
+    int i,j,k;
+
+    for( i=0;i<=10;i++)
+    {
+        for( j=0;j<=10;j++)
+        {
+            if(j==0 || j==10 || i==j)
+            cout << "*";
+            else {
+                
+            }
+            cout<<" ";
+        }
+        cout << "\n";
+    }
+return 0;
+}
+
+
+
+
+
+
+
