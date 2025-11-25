025377d1daec60a3c90bad5655278900b604cb75
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-25 17:26:27 +0530

    Delete all files

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-  int main() {
-int x;
-cout << "enetr value of x";
-cin >> x;
-cout << "value of x:" << x;
-return 0;
-  }
--- strings.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
- string s = "ommaurya";
- int len= s.size();
- cout<<s[len-1];   
-}
