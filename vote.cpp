1383606938c600e323ee6fb7c24641b76cb2e09e
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-25 16:34:09 +0530

    Moved files to pattern folder

---
+++ vote.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() { int age;
+    cout <<"enetr your age ";
+  cin>> age;
+    if (age >=18) {
+        cout <<"you can vote";
+      
+    }
+    else {
+        cout<<"you cannot vote";
+    }
+    
+    return 0;
+  
+
+}
