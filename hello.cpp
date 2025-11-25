e92bda22783af05190a649c23b2b6e9ab74e9abd
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-25 16:58:30 +0530

    str

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+  int main() {
+int x;
+cout << "enetr value of x";
+cin >> x;
+cout << "value of x:" << x;
+return 0;
+  }
---
+++ hello1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,sum=0;
+    cout<<"value of x and y:";
+    cin >>x >> y;
+    sum=x+y;
+    cout<<"sum of number is;"<< sum;
+    return 0;
+}
