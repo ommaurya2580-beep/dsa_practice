116516336b2e62cae4073fc074479aa6e32be7c2
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-26 22:05:56 +0530

    Add 123 pattern

---
+++ 123_pattern.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    int i,j;
+    for (i=1;i<=n;i++)
+    {
+        for (j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+    
+}
+int main(){
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    print(n);
+    return 0;
+}
