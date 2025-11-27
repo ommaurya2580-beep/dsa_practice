4323ff9aaa5fb036f03d57b2f60b9ec02f15d5fb
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-27 18:40:29 +0530

    Add inverted right angle triangle program

---
+++ inverted_right_angle_triange.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+   
+    for (int i=n;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+}
+int main(){
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    print(n);
+    return 0;
+
+}
