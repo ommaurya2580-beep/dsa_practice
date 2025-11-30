b427bdd6002482ffb0fb9fd8cc2f0d3894b31922
ommaurya2580-beep <ommaurya2580@gmail.com>
2025-11-30 20:22:23 +0530

    Add AABABC_triangle.cpp

---
+++ AABABC_triangle.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    char ch='A';
+    for (int i=1;i<=n;i++)
+    {
+        for(ch='A';ch<='A'+i;ch++){
+            cout<<ch;
+            
+        }
+        cout<<endl;
+    }
+    
+}
+int main(){
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    print(n);
+    return 0;
+
+}
